d5c6758ee2e2dcc20002ae28a1d11a9b30d8a397
Stan Lewis <slewis@fusesource.com>
2016-11-23 12:00:00 -0500

    Discover all the tests instead and pass them to nodeunit

--- test/run-tests.js
+++ test/run-tests.js
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
 const process = require('process');
 const fs = require('fs');
+const recursive = require('recursive-readdir');
+const _ = require('lodash');
 const content = fs.readFileSync(__dirname + '/../node_modules/nodeunit/bin/nodeunit.json');
 const options = JSON.parse(content);
-options.recursive = true;
 var reporter = undefined;
 if (process.env.CIRCLE_TEST_REPORTS) {
   console.log("Using junit reporter");
@@ -13,6 +14,9 @@ if (process.env.CIRCLE_TEST_REPORTS) {
   console.log("Using console reporter");
   reporter = require('nodeunit').reporters.default;
 }
-console.log("Options: ", options);
-reporter.run(['test'], options);
+recursive('test', ['!test-*'], function(err, files) {
+  console.log("test options: ", options);
+  console.log("test files: ", files);
+  reporter.run(files, options);
+});
 
